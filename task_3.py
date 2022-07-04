1e7ea1e7a15a02f2dc67495af5abf78743cb9f6f
BeastProgramist <dimasmetana@gmail.com>
2022-07-04 16:45:54 +0300

    Update

--- task_3.py
+++ task_3.py
@@ -7,6 +7,21 @@
 #print("Hello,",user_name,b)
 
 
-a = ('Hi, I am a string variable')
-b = 100
-print(f'{a}{b}')
+#a = ('Hi, I am a string variable')
+#b = 100
+#print(f'{a}{b}')
+
+
+#import math
+#a = 100
+#b = math.factorial(a)
+#print(b)
+
+
+#tup = tuple(range(0,101,2))
+#print(tup)
+
+
+#s = [x**2 for x in tup]
+#print(s)
+
