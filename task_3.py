c6c5ec16e7373146d1cc375d542061d442a67fd3
BeastProgramist <dimasmetana@gmail.com>
2022-07-04 18:58:03 +0300

    Homework for lesson 3 is ready 1.1

--- task_3.py
+++ task_3.py
@@ -1,60 +1,61 @@
 #3.1
-#print("Hello world!")
+print("Hello world!")
 
 
 #3.2
-#user_name = input('Enter yoг username: ')
-#b = '!'
-#print("Hello,",user_name,b)
+user_name = input('Enter yoг username: ')
+b = '!'
+print("Hello,",user_name,b)
 
 
 #3.3
-#a = ('Hi, I am a string variable')
-#b = 100
-#print(f'{a}{b}')
+a = ('Hi, I am a string variable')
+b = 100
+print(f'{a}{b}')
 
 
 #3.4
-#import math
-#a = 100
-#b = math.factorial(a)
-#print(b)
+import math
+a = 100
+b = math.factorial(a)
+print(b)
 
 
 #3.5
-#tup = tuple(range(0,101,2))
-#print(tup)
+tup = tuple(range(0,101,2))
+print(tup)
 
 
 #3.6
-#s = [x**2 for x in tup]
-#print(s)
+s = [x**2 for x in tup]
+print(s)
 
 
 #3.7
-#a = 'sounds/lofi/chillstep.wav'
-#b = a[6:]
-#c = 'midi'+b
-#print(c[19:])
+a = 'sounds/lofi/chillstep.wav'
+b = a[6:]
+c = 'midi'+b
+print(c[19:])
 
 
 #3.8
-#a = [1, 1, 2, 3, 5, 8, 10, 10]
-#print(set(a))
+a = [1, 1, 2, 3, 5, 8, 10, 10]
+print(set(a))
 
 
 #3.9
-#b = [x+1 for x in a]
-#print(b)
+b = [x+1 for x in a]
+print(b)
 
 
 #3.10
-#a = 'Python is the most popular programming language'
-#b = a.count('p')
-#print(b)
+a = 'Python is the most popular programming language'
+b = a.count('p')
+print(b)
 
 
 #3.11
-#a = [0, 2, 3, 4]
-#b = [2, 2, 5]
-#print(list(set(a) - set(b)))
+a = [0, 2, 3, 4]
+b = [2, 2, 5]
+print(set(a) - set(b))
+
